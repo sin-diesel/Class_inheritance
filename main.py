1ac45d5652895435530b6f0162b9c157c426dd07
Stas_SD <sidelnikov.si@phystech.edu>
2020-08-22 13:49:58 +0300

    Version with constructor and body_whl as property

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from solution import *
 
-new_car = CarBase("Cherokee", "photo_path", "2500kg", "Jeep")
+
+truck_dimms = (23.25, 84.21, 9.00)
+new_car = Truck("Kamaz", "photo_path.jpg", "5000kg", truck_dimms)
 new_car.get_info()
--- solution.py
+++ solution.py
@@ -6,13 +6,13 @@ DEBUG = True
 
 class CarBase:
 
-    def __init__(self, brand, photo_file_name, carrying, car_type=None):
+    def __init__(self, car_type=None, brand=None, photo_file_name=None, carrying=None):
         if DEBUG:
-            print("Initializing CarBase object: ")
+            print("\n\n\n Initializing CarBase object: ")
             print("Car type: ", car_type)
             print("Brand: ", brand)
             print("Photo file name: ", photo_file_name)
-            print("Carrying: ", carrying, "\n")
+            print("Carrying: ", carrying)
 
         self._car_type = car_type
         self._photo_file_name = photo_file_name
@@ -20,12 +20,76 @@ class CarBase:
         self._carriyng = carrying
 
     def get_photo_file_ext(self):
-        pass
+        splitted = splitext(self._photo_file_name)[1]
+
+        if DEBUG:
+            print("Splittex extension: ", splitted)
 
     def get_info(self):
-        print("CarBase object information:")
+        print("\n\n\n CarBase object information:")
         print("Car type: ", self._car_type)
         print("Brand: ", self._brand)
         print("Photo file name: ", self._photo_file_name)
         print("Carrying: ", self._carriyng)
+
+
+class Truck(CarBase):
+
+
+    def __init__(self, brand=None, photo_file_name=None, carrying=None, body_whl=None):
+
+        super().__init__("truck", brand, photo_file_name, carrying)
+
+        if DEBUG:
+            print("Initializing truck dimensions with list: ", body_whl)
+            
+        if type(body_whl[0]) == float and type(body_whl[1]) == float and type(body_whl[2]) == float:
+            if DEBUG:
+                print("\n\n\nCorrect dimensions input.")
+            self._body_length = body_whl[0]
+            self._body_width = body_whl[1]
+            self._body_height = body_whl[2]
+        else:
+            self._body_length = 0
+            self._body_width = 0
+            self._body_height = 0
+
+    def get_info(self):
+        super().get_info()
+        print("Truck dimensions: ", self._body_length, self._body_width, self._body_height)
+
+
+    body_whl = property()
+
+    @body_whl.setter
+    def body_whl(self, data):  # check if list contains valid float numbers
+        if DEBUG:
+            print("\n\n\nSetting dimensions property...")
+
+        if type(data[0]) == float and type(data[1]) == float and type(data[2]) == float:
+            if DEBUG:
+                print("\n\n\nCorrect dimensions input.")
+            self._body_length = data[0]
+            self._body_width = data[1]
+            self._body_height = data[2]
+        else:
+            self._body_length = 0
+            self._body_width = 0
+            self._body_height = 0
+
+    @body_whl.getter
+    def body_whl(self):
+        params = list(self._body_length, self._body_width, self._body_height)
+        return params
+
+    @body_whl.deleter
+    def body_whl(self):
+        if DEBUG:
+            print("\n\n\n Deleting Truck object with following object: ")
+            get_info()
+        del self._body_length
+        del self._body_width
+        del self._body_height
+    
+
 
