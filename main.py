30803783f590fd0d3cb5ce98ae39675699f26df4
Stas_SD <sidelnikov.si@phystech.edu>
2020-08-22 14:11:33 +0300

    Now added parsed body_whl

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from solution import *
 
 
-truck_dimms = (23.25, 84.21, 9.00)
+truck_dimms = "23.2x84.2x"
 new_car = Truck("Kamaz", "photo_path.jpg", "5000kg", truck_dimms)
 new_car.get_info()
--- solution.py
+++ solution.py
@@ -4,6 +4,24 @@ from os.path import splitext
 DEBUG = True
 
 
+def parse_dimms(body_whl):
+    assert(isinstance(body_whl, str))
+    parsed = body_whl.split('x')
+
+    if DEBUG:
+        print("\n\n\nParsed values: ", parsed[0], parsed[1], parsed[2])
+
+    try:
+        parsed[0] = float(parsed[0])
+        parsed[1] = float(parsed[1])
+        parsed[2] = float(parsed[2])
+    except ValueError:
+        parsed[0] = 0
+        parsed[1] = 0
+        parsed[2] = 0
+    return parsed
+
+
 class CarBase:
 
     def __init__(self, car_type=None, brand=None, photo_file_name=None, carrying=None):
@@ -35,61 +53,20 @@ class CarBase:
 
 class Truck(CarBase):
 
-
     def __init__(self, brand=None, photo_file_name=None, carrying=None, body_whl=None):
 
         super().__init__("truck", brand, photo_file_name, carrying)
 
         if DEBUG:
-            print("Initializing truck dimensions with list: ", body_whl)
-            
-        if type(body_whl[0]) == float and type(body_whl[1]) == float and type(body_whl[2]) == float:
-            if DEBUG:
-                print("\n\n\nCorrect dimensions input.")
-            self._body_length = body_whl[0]
-            self._body_width = body_whl[1]
-            self._body_height = body_whl[2]
-        else:
-            self._body_length = 0
-            self._body_width = 0
-            self._body_height = 0
+            print("Initializing truck dimensions with string: ", body_whl)
+        params_list = parse_dimms(body_whl)
+        self._body_length = params_list[0]
+        self._body_width = params_list[1]
+        self._body_height = params_list[2]
 
     def get_info(self):
         super().get_info()
         print("Truck dimensions: ", self._body_length, self._body_width, self._body_height)
-
-
-    body_whl = property()
-
-    @body_whl.setter
-    def body_whl(self, data):  # check if list contains valid float numbers
-        if DEBUG:
-            print("\n\n\nSetting dimensions property...")
-
-        if type(data[0]) == float and type(data[1]) == float and type(data[2]) == float:
-            if DEBUG:
-                print("\n\n\nCorrect dimensions input.")
-            self._body_length = data[0]
-            self._body_width = data[1]
-            self._body_height = data[2]
-        else:
-            self._body_length = 0
-            self._body_width = 0
-            self._body_height = 0
-
-    @body_whl.getter
-    def body_whl(self):
-        params = list(self._body_length, self._body_width, self._body_height)
-        return params
-
-    @body_whl.deleter
-    def body_whl(self):
-        if DEBUG:
-            print("\n\n\n Deleting Truck object with following object: ")
-            get_info()
-        del self._body_length
-        del self._body_width
-        del self._body_height
     
 
 
